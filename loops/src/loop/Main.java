afa8124ee006b36d7e6a1ee74dfd959f7f5f5ad8
hilalsariyar <hilalsariyar00@gmail.com>
2022-09-26 13:54:33 +0300

    For and while loops

---
+++ loops/src/loop/Main.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i=1; i<=10; i++) {
+			System.out.println(i);
+			
+		}
+		System.out.println("for döngüsü bitti");
+		
+		int i=1;
+		while (i<10) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("while döngüsü bitti");
+		
+	}
+
+}
